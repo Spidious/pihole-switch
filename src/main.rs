ed8ae066694bc058ef1f3a351bd7edbbaf8a2884
Luke <lukedeffenbaugh@gmail.com>
2024-12-30 21:35:16 -0500

    Add enable and disable options (only disable for 20 seconds)

--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,9 @@ pub mod pi_calls;
 
 enum Message {
     Quit,
-    Green,
-    Red,
+    Disable,
+    Enable,
 }
-
 
 fn main() {
     // Get api key from environment variable
@@ -21,7 +20,7 @@ fn main() {
     let pihole_key = std::env::var("PI_HOLE_KEY").expect("PI_HOLE_KEY must be set");
 
     // Replace the address and key with those of your Pi Hole
-    let _pihole_api = PiHoleAPIConfigWithKey::new(
+    let pihole_api = PiHoleAPIConfigWithKey::new(
         pihole_addr.to_string(),
         pihole_key.to_string(),
     );
@@ -50,15 +49,17 @@ fn main() {
 
     let (tx, rx) = mpsc::sync_channel(1);
 
-    let red_tx = tx.clone();
-    tray.add_menu_item("Red", move || {
-        red_tx.send(Message::Red).unwrap();
+    // Setup disable button
+    let disable_tx = tx.clone();
+    tray.add_menu_item("Disable", move || {
+        disable_tx.send(Message::Disable).unwrap();
     })
     .unwrap();
 
-    let green_tx = tx.clone();
-    tray.add_menu_item("Green", move || {
-        green_tx.send(Message::Green).unwrap();
+    // Setup enable button
+    let enable_tx = tx.clone();
+    tray.add_menu_item("Enable", move || {
+        enable_tx.send(Message::Enable).unwrap();
     })
     .unwrap();
 
@@ -76,15 +77,20 @@ fn main() {
                 println!("Quit");
                 break;
             }
-            Ok(Message::Red) => {
-                println!("Red");
-                tray.set_icon(IconSource::Resource("another-name-from-rc-file"))
-                    .unwrap();
+            Ok(Message::Disable) => {
+                // Handle disable call
+                println!("Disable!!! 20 seconds");
+
+                // 20 second disable
+                pi_calls::disable(&pihole_api, 20);
             }
-            Ok(Message::Green) => {
-                println!("Green");
-                tray.set_icon(IconSource::Resource("name-of-icon-in-rc-file"))
-                    .unwrap()
+            Ok(Message::Enable) => {
+                // Handle enable call
+                println!("Enable");
+                
+                // Handle enable
+                pi_calls::enable(&pihole_api);
+
             }
             _ => {}
         }
--- src/pi_calls.rs
+++ src/pi_calls.rs
@@ -1,13 +1,13 @@
 use pi_hole_api::{AuthenticatedPiHoleAPI, PiHoleAPIConfigWithKey};
 
-pub async fn disable(piapi: PiHoleAPIConfigWithKey) {
-    match piapi.disable(10){
+pub fn disable(piapi: &PiHoleAPIConfigWithKey, seconds: u64) {
+    match piapi.disable(seconds){
         Ok(status) => println!("Disable Success: {:?}", status),
         Err(e) => panic!("Request to disable pihole failed {:?}", e),
     };
 }
 
-pub async fn enable(piapi: PiHoleAPIConfigWithKey) {
+pub fn enable(piapi: &PiHoleAPIConfigWithKey) {
     match piapi.enable(){
         Ok(status) => println!("Enable Success: {:?}", status),
         Err(e) => panic!("Request to enable pihole failed {:?}", e),
